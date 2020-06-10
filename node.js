06b217d05e438889712910129a03805802797951
FellowHead <thefellowhead@gmail.com>
2020-06-10 13:28:01 +0200

    forget about the extra check

--- node.js
+++ node.js
@@ -6,4 +6,4 @@ exports.electronVersion = process.versions.electron || '0.0.0';
 
 exports.isUsingAsar = require('./source/is-using-asar');
 
-exports.fixPathForAsarUnpack = path => exports.isUsingAsar ? path.replace('app.asar', 'app.asar.unpacked') : path;
+exports.fixPathForAsarUnpack = path => path.replace('app.asar', 'app.asar.unpacked');
